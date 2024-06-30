f1034f461c0efb1e7c46ff927ca9eaeed589d170
Yahia Ragae <yahia@mrxprt.com>
2024-07-01 08:38:28 +1200

    - Fix project file and add pods

---
+++ ios/BrightcoveImaPlayerTest/BrightcoveImaPlayerTest.m
@@ -0,0 +1,37 @@
+//
+//  BrightcoveImaPlayerTest.m
+//  BrightcoveImaPlayerTest
+//
+//  Created by Yahia on 26/06/2024.
+//  Copyright © 2024 Facebook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BrightcoveImaPlayerTest : XCTestCase
+
+@end
+
+@implementation BrightcoveImaPlayerTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
